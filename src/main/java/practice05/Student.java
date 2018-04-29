755d392e1f79561ed6f716192965bd9bb68c64cc
llr392425595 <392425595@qq.com>
2018-04-29 16:17:12 +0800

    refactor Pravtice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,23 +1,24 @@
 package practice05;
 
 public class Student extends Person {
-    private int klass;
 
-    public int getKlass() {
-        return klass;
-    }
+  private Integer klass;
 
-    public void setKlass(int klass) {
-        this.klass = klass;
-    }
+  public Student(String name, Integer age, Integer klass) {
+    super(name, age);
+    this.klass = klass;
+  }
 
-    public Student(String name, int age, int klass) {
-        super(name, age);
-        this.klass = klass;
-    }
+  public Integer getKlass() {
+    return klass;
+  }
 
-    @Override
-    public String introduce() {
-        return super.introduce() + String.format(" I am a Student. I am at Class %d.",this.klass );
-    }
+  public void setKlass(Integer klass) {
+    this.klass = klass;
+  }
+
+  @Override
+  public String introduce() {
+    return super.introduce() + String.format(" I am a Student. I am at Class %d.", this.klass);
+  }
 }
